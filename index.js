17cfc9669ecc7ce7e8a2bba18004c5546b6b330c
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-07 18:09:29 +0600

    db user and db password connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors'); 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,6 +9,20 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// bikestore
+// JS96rmgWJ3lB05TO
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvtj0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('bikestore connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 app.get('/',(req,res)=>{
