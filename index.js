870f55d3014f4dfe12731fb2441ce16aac2101c9
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-07 20:57:06 +0600

    api created

--- index.js
+++ index.js
@@ -16,13 +16,22 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvtj0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('bikestore connected');
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+try{
+    await client.connect();
+    const stockCollection = client.db('bikestore-different-brand').collection('stock');
+   app.get('/stock',async(req,res)=>{
+    const query = {};
+    const cursor = stockCollection.find(query);
+    const stockes = await cursor.toArray();
+    res.send(stockes);
+   })
+}
+finally{
 
+}
+}
+run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
