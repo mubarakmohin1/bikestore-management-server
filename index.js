63bf4e80c7ed3db5ca89c078b6b17b77da545164
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-07 21:30:25 +0600

    single id base apicreate

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors'); 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -25,6 +25,12 @@ try{
     const cursor = stockCollection.find(query);
     const stockes = await cursor.toArray();
     res.send(stockes);
+   });
+   app.get('/stock/:id',async(req,res)=>{
+       const id = req.params.id;
+       const query = {_id:ObjectId(id)};
+       const stock = await stockCollection.findOne(query);
+       res.send(stock);
    })
 }
 finally{
